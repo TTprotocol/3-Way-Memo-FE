34019fba60019d4f9c3f08260de8dd935a70b434
TTprotocol <87603518+TTprotocol@users.noreply.github.com>
2026-01-06 20:05:25 +0900

    feat: redux가 반영되는 page.tsx에 설명 주석 추가

--- src/app/redux-memo/page.tsx
+++ src/app/redux-memo/page.tsx
@@ -1,24 +1,24 @@
 "use client";
 
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState, AppDispatch } from "@/store/redux/store";
-import { fetchMemos, addMemo } from "@/store/redux/memoSlice";
+import { useDispatch, useSelector } from "react-redux"; // store에 접근하기 위한 훅
+import { RootState, AppDispatch } from "@/store/redux/store"; // RootState : 전역 상태 타입, AppDispatch : dispatch 타입
+import { fetchMemos, addMemo } from "@/store/redux/memoSlice"; // memoSlice에서 정의한 액션들
 
 export default function ReduxMemoPage() {
 	const [input, setInput] = React.useState("");
-	const dispatch = useDispatch<AppDispatch>();
-	const { memos } = useSelector((state: RootState) => state.memos);
+	const dispatch = useDispatch<AppDispatch>(); // store.dispatch를 가져온다.
+	const { memos } = useSelector((state: RootState) => state.memos); // useSelector로 store의 state에서 memos 슬라이스를 구독한다.
 
 	React.useEffect(() => {
-		dispatch(fetchMemos());
+		dispatch(fetchMemos()); // 메모 목록을 조회하는 액션 함수 실행.
 	}, [dispatch]);
 
 	const submit = async (e: React.FormEvent) => {
 		e.preventDefault();
 		if (!input.trim()) return;
 
-		await dispatch(addMemo(input));
+		await dispatch(addMemo(input)); // 메모를 추가하는 액션 함수 실행
 		setInput("");
 	};
 
@@ -41,6 +41,7 @@ export default function ReduxMemoPage() {
 				</button>
 			</form>
 			<div className="space-y-4">
+				{/* memos의 상태가 바뀌면 이 렌더링 결과가 바뀌게 된다. */}
 				{memos.length === 0 ? (
 					<p className="text-center text-gray-500">작성된 메모가 없습니다.</p>
 				) : (
