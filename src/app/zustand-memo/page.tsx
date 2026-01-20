5e3d1a3e21766582c692716176d7093511e9ab43
TTprotocol <87603518+TTprotocol@users.noreply.github.com>
2026-01-20 09:35:58 +0900

    fix: zustand className 오류 수정

--- src/app/zustand-memo/page.tsx
+++ src/app/zustand-memo/page.tsx
@@ -77,7 +77,7 @@ export default function ZustandMemo() {
 					className="flex-1 p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-white"
 				/>
 				<button
-					className={`text-white px-6 py-3 rounded-lg hover:bg-amber-700, connected ${connected ? "bg-amber-400" : "bg-gray-500"}`}
+					className={`text-white px-6 py-3 rounded-lg hover:bg-amber-700 ${connected ? "bg-amber-400" : "bg-gray-500"}`}
 					disabled={connected}
 				>
 					저장
